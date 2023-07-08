67f478603be9b0ddb1c3eab98730b97d1accacb8
NIDAMANURI SAI ADARSH <99207348+Adarsh-N123@users.noreply.github.com>
2023-07-08 22:24:09 +0530

    Update db.js

--- db.js
+++ db.js
@@ -11,11 +11,11 @@ require('dotenv').config();
 const Pool = require('pg').Pool;
 
  const pool = new Pool({
-  // user: process.env.PG_USER,
-  // host: process.env.PG_HOST,
-  // password: process.env.PG_PASSWORD,
-  // database: process.env.PG_DATABASE,
-  // port: process.env.PG_PORT
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  port: process.env.PG_PORT,
   connectionString: process.env.DB_link,
     ssl: {
         rejectUnauthorized: false
