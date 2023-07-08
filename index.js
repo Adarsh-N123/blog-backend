b900bfc807123d56fa9b8fc6b54ef4875723682d
NIDAMANURI SAI ADARSH <99207348+Adarsh-N123@users.noreply.github.com>
2023-07-08 23:23:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const postroutes = require("./routes/posts.js");
 const app = express();
 app.use(express.json());
 app.use("/api/posts", postroutes);
+app.get("/api/admins", (req, res) => {
+  const data = { admins: ["adarshkln5@gmail.com","atharva.bhanage.20042@iitgoa.ac.in"] };
+  res.json(data); // Sending a JSON response
+});
 
 app.listen(8000, () => {
   console.log('connected');
