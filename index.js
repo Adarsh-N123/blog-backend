b67f05579409e7d855cbd68b7b73bcfe4dab4db6
NIDAMANURI SAI ADARSH <99207348+Adarsh-N123@users.noreply.github.com>
2023-07-08 23:46:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const postroutes = require("./routes/posts.js");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api/posts", postroutes);
 app.get("/api/admins", (req, res) => {
